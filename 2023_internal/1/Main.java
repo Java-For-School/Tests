ab15718e649d7f05dc28210f93ba207e02dd0b22
SorerBOT <alonfiller@gmail.com>
2023-05-21 15:24:07 +0300

    1

---
+++ 2023_internal/1/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+  public static jumpNum(int num) {
+    boolean last = (num % 10) % 2 == 0;
+
+    while (num > 0) {
+      boolean current = (num % 10) % 2 == 0;
+      if (current == last) return false;
+      last = current; 
+    }
+
+    return true;
+  }
+
+  public static void jumpArr(int[] arr) {
+    int[] newArr = new int[arr.length]; 
+    int index = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (jumpNum(arr[i])) {
+        newArr[index] = arr[i];
+        index++
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      if (!jumpNum(arr[i])) {
+        newArr[index] = arr[i];
+        index++
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) arr[i] = newArr[i];
+  }
+}
